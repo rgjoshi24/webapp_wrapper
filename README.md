04b3e9cf8bd7d771025f08a3a85c491f89a2a79e
ChefDK User <chef@ip-172-31-16-179.ec2.internal>
2018-08-21 21:23:27 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# my-webapps
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: my-webapps
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
